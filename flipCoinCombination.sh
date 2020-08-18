399e2170103f5aeca02867a6fee08c146fd6b818
anuja-garud <anujagarud24@gmail.com>
2020-08-18 23:47:12 +0530

    Check for Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -12,3 +12,19 @@ else
     echo "TAIL";
 fi
 
+declare -A coinDictionary
+coinDictionary[HEAD]=0
+coinDictionary[TAIL]=0
+
+for (( i=0 ; i<10 ; i++ ))
+do
+        check=$((RANDOM%2))
+        if [ $check -eq 0 ]
+        then
+                ((coinDictionary[HEAD]++))
+        else
+                ((coinDictionary[TAIL]++))
+        fi
+done
+echo "Percentage of heads is $((${coinDictionary[HEAD]}*100/10))%"
+echo "Percentage of tails is $((${coinDictionary[TAIL]}*100/10))%"
